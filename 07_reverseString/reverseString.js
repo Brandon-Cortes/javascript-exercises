5956e0c7669e550bc52291d78898bc6eeffd5062
Brandon Cortes <brandonozielcortes@gmail.com>
2025-06-25 19:49:06 -0600

    finished up 07 without help

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+    let newText = text.split("");
+    let reverse = "";
+    for (let i = newText.length - 1; i >= 0; i--) {
+        reverse += newText[i];
+        }
+    return reverse;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
